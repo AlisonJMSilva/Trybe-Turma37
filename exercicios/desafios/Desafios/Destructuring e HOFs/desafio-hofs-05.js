df1fd75b02b0516ef2d0a1e4ae1c336a77a9134d
alisonjosems@gmail.com <alisonjosems@gmail.com>
2023-09-13 19:04:04 -0300

    mistakes

--- exercicios/desafios/Desafios/Destructuring e HOFs/desafio-hofs-05.js
+++
@@ -1,9 +0,0 @@
-const data = require('./data');
-
-// 5 - Filtre e retorne um array com os nomes das bandas que contenham 'Rock' no gênero musical
-
-const rockFilter = () => {
-
-}
-
-module.exports = rockFilter;
